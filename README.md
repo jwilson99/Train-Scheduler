75c1c90536e14a6b32278c5b2a810917ef41be3b
jwilson99 <juliewilsongt@gmail.com>
2017-09-27 22:39:58 -0400

    added media queries to prevent table spill-over, added comments to code for clarity, added text to README.md

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Train-Scheduler
+# Train-Scheduler:
+
+# On page load, existing train data will populate in the page table under "Current Train Schedule".
+
+# Input a train name, destination, time of first arrival, and frequency of arrival to update the page.
+
+# The pages updates a table to include the train name, destination, and frequency. It also calculates the time of the next train arrival and the minutes until that arrival.
+
+# All of this data is stored in Google Firebase for later retrieval.
+
+# The time data must be entered as the format indicates, or an alert will appear and the data will not be stored.
--- assets/js/scheduler.js
+++ assets/js/scheduler.js
@@ -17,8 +17,10 @@ $(document).ready(function(){
         firstTrainTime = $("#firstTrainTime").val().trim();
         frequency = $("#frequency").val().trim();
 
+        //all input values must be filled
         if (trainName === "" || destination === "" || firstTrainTime === "" || frequency === "") {
             alert("Please fill in all fields!");
+            //time data must be in correct format in input
         } else if (moment(firstTrainTime,"HH:mm",true).isValid() && (frequency%1) === 0) {
             //pushes data to the firebase database
             database.ref().push({
@@ -38,34 +40,44 @@ $(document).ready(function(){
 
     });
 
+    //when data is added to firebase
     database.ref().orderByChild("timeAdded").on("child_added", function(snapshot) {
+        //initialize variables
         var snapshotData = snapshot.val();
 
+        //current time of user at time of button click
         var currentTime = moment();
 
+        //formats first train data
         var firstTrain = moment(snapshotData.firstTrainTime, "HH:mm");
 
+        //uses the data to calculate time until next train and the time of the next train
         var diffTime = moment().diff(firstTrain, "minutes");
         var timeRemainder = diffTime % snapshotData.frequency;
         var minutesUntilTrain = snapshotData.frequency - timeRemainder;
         var nextTrain = moment().add(minutesUntilTrain,"minutes");
 
+        //creates a new table row to store the data
         var newRow = $("<tr>");
 
+        //creates new table data elements to store data
         var newTrainNameData = $("<td>");
         var newDestinationData = $("<td>");
         var newFrequencyData = $("<td>");
         var nextArrivalData = $("<td>");
         var minutesAwayData = $("<td>");
 
+        //appends data to table data elements
         newTrainNameData.append(snapshotData.trainName);
         newDestinationData.append(snapshotData.destination);
         newFrequencyData.append(snapshotData.frequency);
         nextArrivalData.append(moment(nextTrain).format("HH:mm"));
         minutesAwayData.append(minutesUntilTrain);
 
+        //appends table data elements to table row
         newRow.append(newTrainNameData, newDestinationData,  newFrequencyData, nextArrivalData, minutesAwayData);
 
+        //appends the new table row to the table body
         $("tbody").append(newRow);
     });
 });
